cd244364fee0760bcf872fbdb565df34a296cefd
Dennis Lee <dennislee404@gmail.com>
2023-09-18 16:53:29 +0800

    First Commit

--- app.rb
+++ app.rb
@@ -1,2 +1,32 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+
+require_relative 'models/book'
+
+get '/login' do 
+	erb :login
+end
+
+get '/' do 
+	erb :index, layout: false
+end
+
+post '/add-book' do
+	@book = Book.create(title: params[:title], author: params[:author])
+
+	if @book.save
+		erb :book
+	else
+		erb :index
+	end
+end
+
+get '/books/:id' do 
+	@book = Book.find(params[:id])
+	erb :book
+end
+
+get '/books' do
+	@books = Book.all
+	erb :books
+end
